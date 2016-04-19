34d0549a9d9959e53db17caa7a66306e7c39784f
amtejani <ali@tejani.us>
2016-04-18 21:40:56 -0500

    this should work

--- Theatre.java
+++ Theatre.java
@@ -1,8 +1,31 @@
-package assignment6;
 
-/**
- * Created by Ali on 4/15/2016.
- */
 public class Theatre {
+	private char currentSeatRow ;
+	private int currentSeatNumber;
+	private int tempAdder;
+	private int sign;
 
+	public  Theatre () {
+		currentSeatRow = 'A';
+		currentSeatNumber = 114;
+		tempAdder = 1;
+		sign = 1;
+	}
+
+	public String getNextSeat() {
+		String output = currentSeatRow + "" + currentSeatNumber;
+		if (currentSeatRow <= 'Z') {
+			currentSeatNumber += tempAdder * sign;
+			sign = -1 * sign;
+			tempAdder = (tempAdder + 1) % 28;
+			if(tempAdder == 1) {
+				currentSeatNumber = 114;
+				currentSeatRow += 1;
+				sign = 1;
+			}
+		} else {
+			output = "no empty seats";
+		}
+		return output;
+	}
 }
